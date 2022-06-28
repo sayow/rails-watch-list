0cee9708c14923748f24c38f8e726b8dd0327106
Sayonara Silva <sayow@hotmail.com>
2022-06-28 12:19:30 +0100

    spec OK para models

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,3 +1,6 @@
 class List < ApplicationRecord
-  belongs_to :movie
+  has_many :bookmarks, dependent: :destroy
+  has_many :movies, through: :bookmarks
+
+  validates :name, uniqueness: true, presence: true
 end
