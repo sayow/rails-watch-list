aeaf820b966c226f1a7933b44606c7a51ac5ef8b
Sayonara Silva <sayow@hotmail.com>
2022-07-06 15:42:03 +0100

    Lists controller ok

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,3 +1,5 @@
 class BookmarksController < ApplicationController
 
+
+
 end
--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,2 +1,37 @@
 class ListsController < ApplicationController
+  before_action :set_list, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @lists = List.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+    @list = List.new(list_params)
+    if @list.save
+      redirect_to lists
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @list = List.delete(list_params)
+    redirect_to lists_path
+  end
+
+  private
+
+  def set_list
+    @lists = List.find(params[:id])
+  end
+
+  def list_params
+    params.require(:list).permit(:name)
+  end
 end
