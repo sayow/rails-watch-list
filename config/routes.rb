b731e3fc1a3417f6e56fdb21d5f1cdd7c6a71b72
Sayonara Silva <sayow@hotmail.com>
2022-07-07 11:26:03 +0100

    navbar ok

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'lists#index'
-  resources :lists, only: [ :index, :show, :new, :create, :destroy ] do
+  get "my_lists_of_movies", to:'lists#index'
+  resources :lists, only: [ :show, :new, :create, :destroy ] do
     resources :bookmarks, only: [:new, :create]
   end
   resources :bookmarks, only: [:destroy]
